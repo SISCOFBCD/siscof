63c416356f8fabdca2c9c3b27b1f1421521f9c82
trarrigoni <tr.arrigoni@gmail.com>
2017-06-09 19:40:32 -0300

    Ver pasta final

--- relatorio_pdf/final/inicia.php
+++ relatorio_pdf/final/inicia.php
@@ -3,6 +3,6 @@
 require_once('fpdf/fpdf.php');
 require_once('banco_mysql.php');
 require_once('banco_oracle.php');
-require_once('aplicacao.php'); 
+require_once('relatorio.php');
 
 ?>
--- relatorio_pdf/final/principal.php
+++ relatorio_pdf/final/principal.php
@@ -3,14 +3,14 @@
 require('inicia.php');
 
 	$doc = new Relatorio();
-	
-	$filtro = 2; //id do alerta
-	$ocorrencia = 4; //id da ocorrencia 
+
+	$filtro = 16;
+	$ocorrencia = 2;
 	
 	//$doc=$doc->relatorio_aluno_falta($filtro,$ocorrencia);
 	$doc=$doc->relatorio_turma_falta($filtro,$ocorrencia);
 
-	//$doc=$doc->relatorio_aluno_horario($filtro,$ocorrencia);
+	//$doc=$doc->relatorio_aluno_horario($filtro,$ocorrencia); // 3 -> entrada , 1 ou 2 -> saida
 	//$doc=$doc->relatorio_turma_horario($filtro,$ocorrencia);
 	
 	$doc->Output();
